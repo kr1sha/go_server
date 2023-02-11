e178067a13789735da8f2b1ddf73372bdbcb277a
kr1sha <elustov@mail.ru>
2023-02-11 18:42:12 +0300

    add get post

--- main.go
+++ main.go
@@ -1,16 +1,42 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	_ "github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5"
+	"io/ioutil"
+	"net/http"
+	"strconv"
 )
 
 type Message struct {
-	userName    string
-	messageText string
-	timeStamp   string
+	UserName    string
+	MessageText string
+	TimeStamp   string
 }
 
 func main() {
-	fmt.Print("hello")
+
+	router := chi.NewRouter()
+	fmt.Println("hello")
+	messages := make([]Message, 0)
+
+	router.Post("/api/Messanger", func(writer http.ResponseWriter, request *http.Request) {
+		message := Message{}
+		raw, _ := ioutil.ReadAll(request.Body)
+		json.Unmarshal(raw, &message)
+		fmt.Println(message)
+		messages = append(messages, message)
+	})
+
+	router.Get("/api/Messanger/{id}", func(writer http.ResponseWriter, request *http.Request) {
+		id := chi.URLParam(request, "id")
+		idInt, _ := strconv.Atoi(id)
+		if idInt >= 0 && idInt < len(messages) {
+			message := messages[idInt]
+			data, _ := json.Marshal(message)
+			writer.Write(data)
+		}
+	})
+	http.ListenAndServe(":5000", router)
 }
